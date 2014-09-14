2cb23e340de6d163200a1b582cdda9912f1a318f
Niels Vroegindeweij <Niels@MacBook-Pro-van-Niels-Vroegindeweij.local>
2014-09-14 11:19:16 +0200

    Initial commit

---
+++ SpotifyAlarmClock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SpotifyAlarmClock
+//
+//  Created by Niels Vroegindeweij on 13-09-14.
+//  Copyright (c) 2014 Niels Vroegindeweij. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SpotifyAlarmClockTests/SpotifyAlarmClockTests.m
@@ -0,0 +1,34 @@
+//
+//  SpotifyAlarmClockTests.m
+//  SpotifyAlarmClockTests
+//
+//  Created by Niels Vroegindeweij on 13-09-14.
+//  Copyright (c) 2014 Niels Vroegindeweij. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SpotifyAlarmClockTests : XCTestCase
+
+@end
+
+@implementation SpotifyAlarmClockTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
