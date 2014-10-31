947ca264a857c6a1b9f9632519a238da80633a64
Niels Vroegindeweij <niels@MacBook-Pro-van-Niels.local>
2014-10-31 12:01:05 +0100

    About View Controller added and renamed app

---
+++ SpotifyAlarmClock/Controllers/About/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  Alarm Clock
+//
+//  Created by Niels Vroegindeweij on 31-10-14.
+//  Copyright (c) 2014 Niels Vroegindeweij. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UITableViewController
+
+@end
