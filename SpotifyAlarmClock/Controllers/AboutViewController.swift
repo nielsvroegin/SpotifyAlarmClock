e34b043e759b67a07dbe619e3b4cf91d73c92d27
Niels Vroegindeweij <niels@MacBook-Pro-van-Niels.local>
2015-03-09 21:35:06 +0100

    Introduced swift code, and rewritten AboutViewController to swift

---
+++ SpotifyAlarmClock/Controllers/AboutViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AboutViewController.swift
+//  Alarm Clock
+//
+//  Created by Niels Vroegindeweij on 09-03-15.
+//  Copyright (c) 2015 Niels Vroegindeweij. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UITableViewController {
+
+    @IBOutlet weak var lbVersion : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lbVersion.text = (NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as String)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        switch(indexPath.row) {
+        case 2:
+            UIApplication.sharedApplication().openURL(NSURL(string: "https://www.linkedin.com/pub/niels-vroegindeweij/21/980/24b")!)
+        case 3:
+            UIApplication.sharedApplication().openURL(NSURL(string: "http://alarmclock.startsmart.nl")!)
+        default:
+            break
+        }
+    }
+
+}
